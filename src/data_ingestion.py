366fa535d976c0d04883250732a4d7bcc3ec96ad
Archana Jha <archanajha9231@gmail.com>
2025-05-19 20:37:16 +0530

    added params.yaml with defined parms to alter dynamic variable like test_size, learning_rate

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,7 +3,12 @@ import numpy as np
 import pandas as pd
 import os
 
+import yaml
+
 from sklearn.model_selection import train_test_split
+
+# loading our test_size value from yaml file
+test_size = yaml.safe_load(open('params.yaml', 'r'))['data_ingestion']['test_size']
 
 #code to bring data from url or data source
 df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
@@ -16,7 +21,7 @@ final_df = df[df['sentiment'].isin(['happiness', 'sadness'])]
 final_df['sentiment'].replace({'happiness': 1, 'sadness': 0}, inplace=True)
 
 # also we will do train test split
-train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
 
 # we will maintain a raw copy of data inside data/raw/train.csv and data/raw/test.csv
 data_path = os.path.join("data", "raw")
--- src/model_building.py
+++ src/model_building.py
@@ -4,6 +4,12 @@ import pickle
 
 from sklearn.ensemble import GradientBoostingClassifier
 
+import yaml
+load_yaml = yaml.safe_load(open('params.yaml', 'r'))
+n_estimators = load_yaml['model_building']['n_estimators']
+learning_rate = load_yaml['model_building']['learning_rate']
+
+
 # fetch the data from data/features
 train_data = pd.read_csv('./data/features/train_bow.csv')
 
@@ -12,7 +18,7 @@ y_train = train_data.iloc[:,-1].values
 
 # Define and train the XGBoost model
 
-clf = GradientBoostingClassifier(n_estimators=50)
+clf = GradientBoostingClassifier(n_estimators=n_estimators, learning_rate=learning_rate)
 clf.fit(X_train, y_train)
 
 # save
